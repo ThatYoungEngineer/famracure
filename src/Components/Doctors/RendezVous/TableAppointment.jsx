f411ad32f8ebab9dd63156f473f73f7abd6bef72
Muhammad Talha <95286774+ThatYoungEngineer@users.noreply.github.com>
2025-05-13 14:08:51 +0500

    Update TableAppointment.jsx

--- src/Components/Doctors/RendezVous/TableAppointment.jsx
+++ src/Components/Doctors/RendezVous/TableAppointment.jsx
@@ -146,7 +146,7 @@ const TableAppointment = ({ refreshApp, showAnnuler, setShowAnnuler, setIdAppoin
   }
   const downloadPrescription = (id) => {
     axiosClient
-      .get(`/prescriptions/${id}/download`, { responseType: "blob" })
+      .get(`/prescriptions/download/${id}`, { responseType: "blob" })
       .then((res) => {
         console.log('res of download prescription: ', res)
         const url = window.URL.createObjectURL(new Blob([res.data], { type: 'application/pdf' }));
